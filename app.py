9a35a2871d4d5c10298ba56c4a64dfdc0eeaf586
Дмитрий <kovtundmitriy11@gmail.com>
2025-05-09 14:11:44 +0000

    Добавлен ML-пайплайн

---
+++ app.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, File, UploadFile
+import pandas as pd
+import joblib
+from io import BytesIO
+import os
+
+app = FastAPI()
+
+# Укажите путь к модели (обязательно сохраните её туда заранее)
+model_path = '/content/drive/MyDrive/MTUCI/laptop_price_model.pkl'
+model = joblib.load(model_path)
+
+@app.post("/predict/")
+async def predict(file: UploadFile = File(...)):
+    content = await file.read()
+    df = pd.read_csv(BytesIO(content))
+    predictions = model.predict(df)
+    return {"predictions": predictions.tolist()}
